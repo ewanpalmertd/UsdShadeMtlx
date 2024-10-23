5d112fd72f4bd5b8719773a1454a12bbafd09bb5
ewanpalmertd <ejp.wrk@gmail.com>
2024-10-23 22:04:47 +0100

    removed old files

--- src/UsdShadeMtlx/UsdShadeMtlx.py
+++
@@ -1,9 +0,0 @@
-import json
-from pxr import Usd, Sdf, UsdShade
-
-class MtlxShade:
-
-    def __init__(self, layer, path:str, nodeType:str) -> None:
-        self.layer = layer
-        self.path  = path
-        self. nodeType = nodeType
