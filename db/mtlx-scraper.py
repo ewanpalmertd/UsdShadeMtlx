36fd92ccd277b1ef1e79b4fe37a873cb87c06c37
ewanpalmertd <ejp.wrk@gmail.com>
2024-10-29 14:11:07 +0000

    changed scraper functionality

--- db/mtlx-scraper.py
+++ db/mtlx-scraper.py
@@ -1,18 +1,33 @@
-import requests
-from bs4 import BeautifulSoup as bs
-from selenium import webdriver
-from selenium.webdriver import Firefox
+# TODO
+# split the mtlx file into sections for each node
+# write efficient data parser to get {id, inputs:[type, default], outputs:[type, default]}
+
+import re
+import os
+import logging
+from pathlib import Path
 from typing import List, Dict, Optional
 
-URL: str = "https://github.com/AcademySoftwareFoundation/MaterialX"
-page = requests.get(URL)
+STDLIB: str = str(Path(__file__).with_name("stdlib_defs.mtlx"))
 
 
-def get_page(url: str = URL) -> str:
-    # get the raw html from input page
-    assert url
-    return requests.get(url).text
+def split_mtlx_definitions(input_file: str = STDLIB) -> List[str]:
+    # splitting the stdlib.mtlx file into each of their node
+    # definitions then returning as a list
+
+    if not os.path.isfile(input_file):
+        logging.exception("File does not exist")
+
+    # would like to try to find an alternative method at some point
+    output_list: List[str] = []
+    with open(input_file) as file:
+        contents = file.read().split("</nodedef>")
+        for line in contents:
+            output_list.append(f"<nodedef {line.split('<nodedef ')[-1]}")
+
+    return output_list
 
 
 if __name__ == "__main__":
-    print(page.text)
+    split_list = split_mtlx_definitions()
+    print(split_list[0])
