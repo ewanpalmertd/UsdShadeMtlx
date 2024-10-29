f82e6fc8f9837fac8d8893a03d5a9cd415c71a5e
ewanpalmertd <ejp.wrk@gmail.com>
2024-10-29 14:16:41 +0000

    updated docstrings

--- db/mtlx-scraper.py
+++ db/mtlx-scraper.py
@@ -12,8 +12,18 @@ STDLIB: str = str(Path(__file__).with_name("stdlib_defs.mtlx"))
 
 
 def split_mtlx_definitions(input_file: str = STDLIB) -> List[str]:
-    # splitting the stdlib.mtlx file into each of their node
-    # definitions then returning as a list
+    """
+    Takes the stdlib_defs.mtlx file from the public repo and splits all
+    definitions into a list to be parsed later
+
+    Args:
+    ----
+        input_file (str): Takes the input .mtlx file to split from
+
+    Returns:
+    ----
+        List[str]: a list of each materialx node definition
+    """
 
     if not os.path.isfile(input_file):
         logging.exception("File does not exist")
@@ -26,6 +36,22 @@ def split_mtlx_definitions(input_file: str = STDLIB) -> List[str]:
             output_list.append(f"<nodedef {line.split('<nodedef ')[-1]}")
 
     return output_list
+
+
+def parse_mtlx_definitions(definitions: List[str]) -> None:
+    """
+    Takes the raw materialx definitions and converts
+    them to more readable data
+
+    Args:
+    ----
+        definitions (List[str]): The list of nodes from the previous function
+
+    Returns:
+    ----
+        !!still need to figure out the best way to return
+    """
+    pass
 
 
 if __name__ == "__main__":
