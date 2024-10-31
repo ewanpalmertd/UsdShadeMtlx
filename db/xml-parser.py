9648c2b7f1516511c4c73e82f630806541025397
ewanpalmertd <ejp.wrk@gmail.com>
2024-10-31 17:03:58 +0000

    wrapped into function/cleaned up script

--- db/xml-parser.py
+++ db/xml-parser.py
@@ -1,34 +1,45 @@
-import xml.dom.minidom
+import logging
 from pathlib import Path
-
-xml_file: str = str(Path(__file__).with_name("test.xml"))
-xml_doc = xml.dom.minidom.parse(xml_file)
-root = xml_doc.documentElement
-package = xml_doc.getElementsByTagName("nodedef")
-
-
+from utils import executeTimeDecorator
+from typing import List, Dict, Optional
 import xml.etree.ElementTree as ET
 
-tree = ET.parse(xml_file)
-root = tree.getroot()
+xml_file: str = str(Path(__file__).with_name("test.xml"))
 
-children = {}
-for child in root:
-    # dont need to worry about child.tag as its just nodedef
-    # print(child.tag, child.attrib)
-    name: str = child.attrib["name"]
-    if not name.startswith("ND"):
-        continue
-    inputs = []
-    outputs = []
-    children[name] = [inputs, outputs]
-    for i in child:
-        io_tag = i.tag
-        io_attrib = i.attrib
+# TODO
+# > check all input and output types to start figuring ways out how to filter them
+# > filter all input/output data and convert it to usdAPI readable data
 
-        if io_tag == "input":
-            inputs.append(io_attrib)
-        elif io_tag == "output":
-            outputs.append(io_attrib)
 
-print(children)
+@executeTimeDecorator
+def parse_xml_file(
+    input_file: str,
+):  # might need to sort return data later, we shall see
+    """
+    Given a .xml file. this function will parse it and get the relevant data
+    in this case, it is parsing all the needed information from the materialx stdlib
+    which has been converted from .mtlx to .xml
+    """
+    tree: ET.ElementTree = ET.parse(input_file)
+    root: ET.Element = tree.getroot()
+    children = {}
+    for child in root:
+        name: str = child.attrib["name"]
+        if not name.startswith("ND"):
+            continue
+        inputs = []
+        outputs = []
+        children[name] = [inputs, outputs]
+        for i in child:
+            io_tag = i.tag
+            io_attrib = i.attrib
+            if io_tag == "input":
+                inputs.append(io_attrib)
+            elif io_tag == "output":
+                outputs.append(io_attrib)
+
+    return children
+
+
+if __name__ == "__main__":
+    parse_xml_file(input_file=xml_file)
