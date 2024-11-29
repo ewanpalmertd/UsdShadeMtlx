8b0d583203522a479a5c9f904f038a922531a6c3
ewanpalmertd <ejp.wrk@gmail.com>
2024-11-29 19:34:10 +0000

    changed functionality to work with new database

--- src/sdfmaterial.py
+++ src/sdfmaterial.py
@@ -1,35 +1,49 @@
-from pxr import Sdf, Usd
+from pxr    import Sdf, Usd
 from typing import List, Any
-from utils import (
+from utils  import (
     time_execution, 
     function_execution_test,
-    str_to_sdfpath)
-from config import USDSHADEMTLX_DATABASE
+    str_to_sdfpath
+)
+
+
+# ------------------------------------------------------------------------------------------------------------
+
 
 class SdfMaterialSpec:
-    def __init__(self, layer:Sdf.Layer, path) -> None:
-        if not isinstance(layer, Sdf.Layer): raise TypeError("Layer must be Sdf.Layer object") # write better error message
-        if not isinstance(path, Sdf.Path):
-            self.path = str_to_sdfpath(path)
-        else:
-            self.path = path 
+    
+    def __init__(self, layer : Sdf.Layer, path):
+        
+        if not isinstance(layer, Sdf.Layer): raise TypeError("Layer must be Sdf.Layer object")
+        self.path  = str_to_sdfpath(path)
         self.layer = layer
 
-    def __setup(self) -> None:
+    def __setup(self):
+    
         self.material = Sdf.CreatePrimInLayer(self.layer, self.path)
         self.material.specifier = Sdf.SpecifierDef
         self.material.typeName  = "Material"
 
-    def AssignToPrimSpec(self, prim_spec:Sdf.PrimSpec, purpose=None) -> None:
-        # sort out preview
+    def AssignToPrimSpec(self, prim_spec : Sdf.PrimSpec, purpose=None):
+    
+        # TODO: add in custom purpose functionality
         material_binding_rel_spec = Sdf.RelationshipSpec(prim_spec, "material:binding", custom=False)
         material_binding_rel_spec.targetPathList.Append(self.path)
-        schemas = Sdf.TokenListOp.Create(
-            appendedItems=["MaterialBindingAPI"]
-        )
+
+        # NOTE: not sure this works if user adds another schema on top
+        schemas = Sdf.TokenListOp.Create(appendedItems=["MaterialBindingAPI"])
         prim_spec.SetInfo("apiSchemas", schemas)
-        return
     
-    def ConnectToShaderSpec(self, shader_spec, source_name) -> None:
-        return
+    def ConnectSurfaceToShaderSpec(self, shader_spec, source_name=None):
+        
+        # NOTE: if source has only one input then just default to input
+        # NOTE: need to account for displacement and other attributes as well, or just automate it
+        if len( shader_spec.database.Outputs() ) == 1: 
+            source_name = "outputs:" + shader_spec.database.Outputs()[0]
+
+        source_path = shader_spec.path.AppendProperty(source_name)
+        material_output_attr_spec = Sdf.AttributeSpec(self.material, "outputs:surface", Sdf.ValueTypeNames.Token)
+        material_output_attr_spec.connectionPathList.Append(source_path)
     
+
+# ------------------------------------------------------------------------------------------------------------
